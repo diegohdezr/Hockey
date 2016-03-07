9eeaa4dff12d35ebfdb19a016d8e9e56ac4303cb
diegohdezr <diego.hdezr@prefixa.com>
2016-03-06 22:23:23 -0600

    started to link the basics of OpenGL, noob work done here!!!!

--- app/src/main/java/com/prefixa/android/hockey/AirHockeyRenderer.java
+++ app/src/main/java/com/prefixa/android/hockey/AirHockeyRenderer.java
@@ -2,7 +2,10 @@ package com.prefixa.android.hockey;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
+import com.prefixa.android.hockey.util.LoggerConfig;
+import com.prefixa.android.hockey.util.ShaderHelper;
 import com.prefixa.android.hockey.util.TextResourceReader;
 
 import java.nio.ByteBuffer;
@@ -13,8 +16,13 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
 
 /**
@@ -27,6 +35,16 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private final FloatBuffer vertexData;
 
     private final Context context;
+
+    private int program;                                    //OpenGL program ID to link the shaders
+    private static final String U_COLOR = "u_Color";        //constant to refer to our color uniform
+    private int uColorLocation;                             //variable to hold the uniform pos in
+                                                            // the OpenGL program, Uniform locations
+                                                            //don't get specified beforehand so we
+                                                            //we need this variable to query its pos later
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;                          //same stuff, like in uniform we store the position
+                                                            //of our attributes here
 
     public AirHockeyRenderer(Context context){
         this.context = context;
@@ -56,6 +74,29 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         glClearColor(1.0f,1.0f,0.0f,0.0f);
         String vertexShaderSource = TextResourceReader.readTextFileFromResource(context,R.raw.simple_vertex_shader);
         String fragmentShaderSource = TextResourceReader.readTextFileFromResource(context,R.raw.simple_fragment_shader);
+
+        int vertexShader = ShaderHelper.complieVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+        program = ShaderHelper.linkProgram(vertexShader,fragmentShader);
+        //validate the program to know useful runtime data about the program
+        if (LoggerConfig.ON){
+            ShaderHelper.validateProgram(program);
+        }
+        //now we use the program which has the shaders linked with our instance of OpenGL
+        glUseProgram(program);
+
+        //query and store the uniform location so that its value can be updated
+        uColorLocation = glGetUniformLocation(program, U_COLOR);
+
+        //query and store the atribute location so it can be updated
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        //find the data for our attribute position
+        vertexData.position(0);
+        //passing incorrect arguments to this function can lead to strange results or even crash the app
+        //so basically debug here if the error is not clear!
+        glVertexAttribPointer(aPositionLocation,POSITION_COMPRONENT_COUNT, GL_FLOAT,false,0, vertexData);
+
     }
 
     @Override
--- app/src/main/java/com/prefixa/android/hockey/util/ShaderHelper.java
+++ app/src/main/java/com/prefixa/android/hockey/util/ShaderHelper.java
@@ -24,6 +24,7 @@ import android.util.Log;
  */
 public class ShaderHelper {
 
+    //IMPORTANT: VALIDATE ONLY IN DEVELOPMENT, STOP IT AFTER RELEASE
     private static final String TAG = "ShaderHelper";
 
     public static int complieVertexShader(String shaderCode){
@@ -42,7 +43,86 @@ public class ShaderHelper {
             }
             return 0;
         }
-        return 1;
+        //upload and compile the shader source to OpenGL
+        glShaderSource(shaderObjectId, shaderCode);
+        glShaderSource(shaderObjectId, shaderCode);
+        //check if the shader compiled correctly
+        final int[] compileStatus = new int[1];
+        glGetShaderiv(shaderObjectId, GL_COMPILE_STATUS, compileStatus, 0);
+        //get a log about the shader if available
+        if (LoggerConfig.ON) {
+            // Print the shader info log to the Android log output.
+            Log.v(TAG, "Results of compiling source:" + "\n" + shaderCode + "\n:"
+                    + glGetShaderInfoLog(shaderObjectId));
+        }
+
+        //verify the shader compile status.
+        if(compileStatus[0] == 0){
+            //if it failed, delete the shader object.
+            glDeleteShader(shaderObjectId);
+
+            if(LoggerConfig.ON){
+                Log.w(TAG,"Compilation of the shader failed");
+            }
+            return 0;
+        }
+
+        return shaderObjectId;
+    }
+    /*links a vertex shader and a fragment shader dogether into an OpenGL
+    * program, it returns the OpenGL program object id, or 0 if the linking failed*/
+    public static int linkProgram(int vertexShaderID, int fragmentShaderID){
+        final int programObjectId = glCreateProgram();
+        if (programObjectId == 0){
+            if (LoggerConfig.ON){
+                Log.w(TAG, "Could not create the new program");
+            }
+            return 0;
+        }
+        //the integer returned is our reference to the proigram object, and we'll get a return
+        //value of 0 if the object creation failed.
+
+        //attach the shaders to the program
+        glAttachShader(programObjectId,vertexShaderID);
+        glAttachShader(programObjectId,fragmentShaderID);
+
+        //link the two shaders
+        glLinkProgram(programObjectId);
+
+        //check if the link was succesfull
+        final int [] linkStatus = new int[1];
+        glGetProgramiv(programObjectId, GL_LINK_STATUS, linkStatus, 0);
+
+        //check log for feedback on link status
+        if(LoggerConfig.ON ){
+            //print the program info log to the Android log output
+            Log.v(TAG,"results of the linking program: \n"+ glGetProgramInfoLog(programObjectId));
+        }
+
+        //as with the shaders we have to delete the linked program if the link failed
+        //so we check the link status, if it is 0 then we cant use the program object, so we
+        //delete it and most important, return 0 to the caller code so it knows we failed.
+        if (linkStatus[0] == 0){
+            //if the link failed, delete the program object
+            glDeleteProgram(programObjectId);
+            if (LoggerConfig.ON){
+                Log.w(TAG,"Linking program failed.");
+            }
+            return 0;
+        }
+        return programObjectId;
     }
 
+
+    /*validates the OpenGL program. should only be called in development, delete for release*/
+    public static boolean validateProgram(int programObjectId){
+
+        glValidateProgram(programObjectId);
+        final int[] validateStatus = new int[1];
+        glGetProgramiv(programObjectId, GL_VALIDATE_STATUS,validateStatus,0);
+        Log.w(TAG,"Results of validating the program: "+ validateStatus[0]
+                + "\nLog: "+ glGetProgramInfoLog(programObjectId));
+
+        return validateStatus[0]!=0;
+    }
 }
